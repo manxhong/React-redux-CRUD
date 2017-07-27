28c83d2d7314ae3091b6d83bf1127e3fea76101f
Man Hong Lee <manhong323@gmail.com>
2017-07-27 15:23:58 -0400

    loading dot animate

--- src/components/common/LoadingDot.js
+++ src/components/common/LoadingDot.js
@@ -3,16 +3,43 @@ import React,{PropTypes} from 'react';
 class LoadingDot extends React.Component{
   constructor(props, context){
     super(props, context);
+
+    this.state = {frame: 1};
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        frame: this.state.frame+ 1
+      });
+    }, this.props.interval);
+  }
+
+  componentWillUnmount() {
+    cleanInterval(this.interval);
   }
 
   render(){
+    debugger;
+    let dots = this.state.frame % (this.props.dots + 1);
+    let text = '';
+    while (dots > 0){
+      text += '.';
+      dots--;
+    }
     return(
-      <div></div>
+      <span {...this.props}>{text}&nbsp;</span>
     );
   }
 }
 
+LoadingDot.defaultProps = {
+  interval: 300, dots: 3
+};
+
 LoadingDot.propTypes={
+  interval: PropTypes.number,
+  dots: PropTypes.number
   //myProp: PropTypes.string.isRequired
 };
 
