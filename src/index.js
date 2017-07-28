4171b17752acc0abf03e3b324a161aca836b62a0
Man Hong Lee <manhong323@gmail.com>
2017-07-28 14:36:18 -0400

    add toastr notification after saving course

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {Provider} from 'react-redux';
 import  routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap-theme.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
 
 const store = configureStore();
 store.dispatch(loadCourses());
