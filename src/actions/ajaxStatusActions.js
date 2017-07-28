7be1b5b3eb70296cbe49ee1baa90b2e2c7d4da4a
Man Hong Lee <manhong323@gmail.com>
2017-07-28 15:10:47 -0400

    handle error

--- src/actions/ajaxStatusActions.js
+++ src/actions/ajaxStatusActions.js
@@ -3,3 +3,7 @@ import * as types from './actionTypes';
 export function beginAjaxCall(){
   return {type: types.BEGIN_AJAX_CALL};
 }
+
+export function createCourseError() {
+  return {type: types.CREATE_COURSE_ERROR};
+}
