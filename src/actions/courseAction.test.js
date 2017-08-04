d857357ae0265f6bad1e0f0d283dc5c570adecf0
Man Hong Lee <manhong323@gmail.com>
2017-08-04 14:25:32 -0400

    add testing thunk

--- src/actions/courseAction.test.js
+++ src/actions/courseAction.test.js
@@ -2,6 +2,10 @@ import expect from 'expect';
 import * as courseActions from './courseActions';
 import * as types from './actionTypes';
 
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
+
 describe('Course Actions', ()=>{
   describe('createCourseSuccess', ()=>{
     it('should create a CREATE_COURSE_SUCCESS action', ()=>{
@@ -17,6 +21,35 @@ describe('Course Actions', ()=>{
 
       //assert
       expect(action).toEqual(expectedAction);
+    });
+  });
+});
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe('Async Actions', ()=>{
+  afterEach(()=> {
+    nock.cleanAll();
+  });
+
+  it('should create BEGIN_AJAX_CALL and LOAD_COURSE_SUCCESS when loading course', (done) => {
+    // Here's an example call to nock.
+    // nock('http://example.com/')
+    //   .get('/courses')
+    //   .reply(200, { body: { course: [{ id: 1, firstName: 'Cory', lastName: 'House'}] }});
+
+    const expectedActions = [
+      {type: types.BEGIN_AJAX_CALL},
+      {type: types.LOAD_COURSES_SUCCESS, body: { course: [{ id : 'clean-code', title: 'Clean Code'}]}}
+    ];
+
+    const store = mockStore({course: []}, expectedActions);
+    store.dispatch(courseActions.loadCourses()).then(() => {
+      const actions = store.getActions();
+      expect(actions[0].type).toEqual(types.BEGIN_AJAX_CALL);
+      expect(actions[1].type).toEqual(types.LOAD_COURSES_SUCCESS);
+      done();
     });
   });
 });
